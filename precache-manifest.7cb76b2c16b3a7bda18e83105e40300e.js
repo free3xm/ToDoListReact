13a6b6ba6d265682db7aa06ef98dcb311c1c019c
free3 <free3xm@gmail.com>
2019-09-28 18:41:01 +0300

    up

---
+++ precache-manifest.7cb76b2c16b3a7bda18e83105e40300e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8b8111e4274e9e81572b6de50574d7",
+    "url": "/ToDoListReact/index.html"
+  },
+  {
+    "revision": "1280cdf0cd255f8f2667",
+    "url": "/ToDoListReact/static/css/main.48802495.chunk.css"
+  },
+  {
+    "revision": "4855a069c27faf8d058f",
+    "url": "/ToDoListReact/static/js/2.6e518c4d.chunk.js"
+  },
+  {
+    "revision": "1280cdf0cd255f8f2667",
+    "url": "/ToDoListReact/static/js/main.af354169.chunk.js"
+  },
+  {
+    "revision": "fb0086b748ca66d9020e",
+    "url": "/ToDoListReact/static/js/runtime-main.229ef5a8.js"
+  },
+  {
+    "revision": "ec6c6237b3da778a325d81e228bbdfc2",
+    "url": "/ToDoListReact/static/media/add.ec6c6237.svg"
+  },
+  {
+    "revision": "be93079ab5051483040a8e4f0b8a9b6a",
+    "url": "/ToDoListReact/static/media/delete.be93079a.svg"
+  },
+  {
+    "revision": "62eea48c78c352815b76289042fe1932",
+    "url": "/ToDoListReact/static/media/deleteall.62eea48c.svg"
+  },
+  {
+    "revision": "061529df191f0946f3f5e48e8b2d4376",
+    "url": "/ToDoListReact/static/media/deleteallhover.061529df.svg"
+  }
+]);
